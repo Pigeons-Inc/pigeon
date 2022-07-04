5140aaca332c5c96884bf8968f228037873384da
Eugene <pashkovskiy.eugen@gmail.com>
2022-07-04 23:28:34 +0300

    init/port-fix

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import path from 'path';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, '../../client/build')));
 
