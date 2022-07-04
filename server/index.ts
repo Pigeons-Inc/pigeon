e877774ef72cf98e52cf3735d1ead118b9be6628
Eugene <pashkovskiy.eugen@gmail.com>
2022-07-04 23:23:52 +0300

    init/typescript-implemented

---
+++ server/index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(express.static(path.resolve(__dirname, '../../client/build')));
+
+app.get('*', (req: Request, res: Response) => {
+  res.status(404).end();
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at https://localhost:${port}`);
+});
