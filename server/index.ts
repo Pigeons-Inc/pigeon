5484912b51f91dfa3a88e530008a30a2ef93ecb2
Eugene <pashkovskiy.eugen@gmail.com>
2022-07-05 02:29:51 +0300

    init/removed-vulnarability

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ import path from 'path';
 dotenv.config();
 
 const app: Express = express();
+app.disable('x-powered-by');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, '../../client/build')));
