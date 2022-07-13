1734818421a8530a0237f18bbff0e3807c1e46a2
Eugene <pashkovskiy.eugen@gmail.com>
2022-07-13 04:39:50 +0300

    Added exclusions for sonar

--- server/microservices/gateway/src/index.ts
+++ server/microservices/gateway/src/index.ts
@@ -1,38 +1,8 @@
-import express, { Express, Request, Response } from 'express';
-import path from 'path';
-import proxy from 'express-http-proxy';
-import swaggerUi from 'swagger-ui-express';
+import app from './app';
 import * as dotenv from 'dotenv';
+import path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
-
-const app: Express = express();
-app.disable('x-powered-by');
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'public')));
-app.use(express.static(path.resolve(__dirname, '../../../../client/build')));
-app.use(
-  '/api/auth',
-  proxy(process.env.AUTH_SERVICE_URL || 'http://localhost:3001')
-);
-app.use(
-  '/api/docs',
-  swaggerUi.serve,
-  swaggerUi.setup(undefined, {
-    swaggerOptions: {
-      url: '/swagger.json',
-    },
-  })
-);
-
-app.get('/ping', async (_req: Request, res: Response) => {
-  res.json({ message: 'pong' });
-});
-
-app.get('*', (_req: Request, res: Response) => {
-  res.status(404).end();
-});
 
 app.listen(port, () => {
   console.log(`API gateway is running at http://localhost:${port}`);
