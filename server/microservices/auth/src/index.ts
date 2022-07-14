ca7ef2e080504025707959faf6284e3229817101
Eugene <pashkovskiy.eugen@gmail.com>
2022-07-14 22:31:32 +0300

    Fixed cluster

--- server/microservices/auth/src/index.ts
+++ server/microservices/auth/src/index.ts
@@ -2,57 +2,49 @@ import db from './repository/sequelize';
 import app from './app';
 import { Server } from 'http';
 import { Socket } from 'net';
-import cluster from 'cluster';
-import os from 'os';
 const port = process.env.PORT || 3001;
 
-if (cluster.isPrimary) {
-  for (let i = 0; i < os.cpus().length; i++) {
-    cluster.fork();
-  }
-} else {
-  db.authenticate()
-    .then(() => {
-      console.log(
-        'Connection to auth database has been established successfully.'
-      );
-      let connections: Socket[] = [];
-      const shutdown = (server: Server) => () => {
-        db.close().then(() => {
-          server.close(() => {
-            process.exit(0);
-          });
+db.authenticate()
+  .then(() => {
+    console.log(
+      'Connection to auth database has been established successfully.'
+    );
+    let connections: Socket[] = [];
+    const shutdown = (server: Server) => () => {
+      db.close().then(() => {
+        server.close(() => {
+          process.exit(0);
         });
-        setTimeout(() => {
-          console.error(
-            'Could not close connections in time, forcefully shutting down'
-          );
-          process.exit(1);
-        }, 10000);
-
-        connections.forEach((curr) => curr.end());
-        setTimeout(() => connections.forEach((curr) => curr.destroy()), 5000);
-      };
-
-      db.sync({ logging: false }).then(() => {
-        const server = app.listen(port, () => {
-          console.log(`Auth service is running at http://localhost:${port}`);
-        });
-
-        server.on('connection', (connection) => {
-          connections.push(connection);
-          connection.on(
-            'close',
-            () =>
-              (connections = connections.filter((curr) => curr !== connection))
-          );
-        });
-
-        process.on('SIGTERM', shutdown(server));
-        process.on('SIGINT', shutdown(server));
       });
-    })
-    .catch((err) => {
-      throw new Error('Unable to connect to the database: ' + err);
+      setTimeout(() => {
+        console.error(
+          'Could not close connections in time, forcefully shutting down'
+        );
+        process.exit(1);
+      }, 10000);
+
+      connections.forEach((curr) => curr.end());
+      setTimeout(() => connections.forEach((curr) => curr.destroy()), 5000);
+    };
+
+    db.sync({ logging: false }).then(() => {
+      const server = app.listen(port, () => {
+        console.log(`Auth service is running at http://localhost:${port}`);
+      });
+
+      server.on('connection', (connection) => {
+        connections.push(connection);
+        connection.on(
+          'close',
+          () =>
+            (connections = connections.filter((curr) => curr !== connection))
+        );
+      });
+
+      process.on('SIGTERM', shutdown(server));
+      process.on('SIGINT', shutdown(server));
     });
-}
+  })
+  .catch((err) => {
+    throw new Error('Unable to connect to the database: ' + err);
+  });
--- server/microservices/gateway/src/index.ts
+++ server/microservices/gateway/src/index.ts
@@ -6,13 +6,21 @@ import os from 'os';
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 const port = process.env.PORT || 3000;
 
+if (+process.version.split('.')[0] < 16)
+  throw new Error('Node version >= 16.x is required');
 if (cluster.isPrimary) {
-  for (let i = 0; i < os.cpus().length; i++) {
-    cluster.fork();
+  console.log();
+  console.log(`API gateway is running at http://localhost:${port}`);
+  console.log(`Starting gateway primary process: ${process.pid}`);
+  for (let i = 0; i < os.cpus().length - 1; i++) {
+    const worker = cluster.fork();
+    worker.on('exit', () => {
+      console.log('Worker exited: ' + worker.process.pid + '. Restarting...');
+      cluster.fork();
+    });
   }
 } else {
-  console.log('Worker started: ' + process.pid);
   app.listen(port, () => {
-    console.log(`API gateway is running at http://localhost:${port}`);
+    console.log('Gateway worker started: ' + process.pid);
   });
 }
